7ddbb52aa6bb980513426313498836c707411242
junddao <y2hunter@gmail.com>
2022-06-23 14:06:32 +0900

    :art: add get records

--- lib/global/provider/record_provider.dart
+++ lib/global/provider/record_provider.dart
@@ -1,10 +1,15 @@
 import 'dart:async';
+import 'dart:collection';
 
+import 'package:intl/intl.dart';
 import 'package:my_workout_diary_app/global/bloc/parent_provider.dart';
 import 'package:my_workout_diary_app/global/model/record/model_record.dart';
+import 'package:my_workout_diary_app/global/model/record/model_record_event.dart';
 import 'package:my_workout_diary_app/global/model/record/model_request_get_records.dart';
 import 'package:my_workout_diary_app/global/model/record/model_response_get_records.dart';
 import 'package:my_workout_diary_app/global/service/api_service.dart';
+import 'package:my_workout_diary_app/global/util/extension/datetime.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class RecordProvider extends ParentProvider {
   late Timer _timer;
@@ -53,5 +58,53 @@ class RecordProvider extends ParentProvider {
     } catch (e) {
       return false;
     }
+  }
+
+  LinkedHashMap<DateTime, List<ModelRecord>> getEvents() {
+    final kToday = DateTime.now();
+
+    List<DateTime> eventDates = [];
+    String tmpDate = '';
+
+    records.forEach((element) {
+      String startDateStr = DateFormat('yyyy-MM-dd').format(element.startTime);
+      if (tmpDate != startDateStr) {
+        eventDates.add(element.startTime);
+
+        tmpDate = startDateStr;
+      }
+    });
+
+    Map<DateTime, List<ModelRecord>> map = {
+      for (var i in List.generate(eventDates.length, (index) => index))
+        eventDates[i]: List.generate(
+          records
+              .where((element) {
+                bool result = element.startTime.toFullDateString3() == eventDates[i].toFullDateString3();
+                return result;
+              })
+              .toList()
+              .length,
+          (index) {
+            print(index);
+            List<ModelRecord> filteredRecords = [];
+            for (var e in records) {
+              if (e.startTime.toFullDateString3() == eventDates[i].toFullDateString3()) {
+                filteredRecords.add(e);
+              }
+            }
+            return filteredRecords[index];
+          },
+        )
+    };
+
+    final result = LinkedHashMap<DateTime, List<ModelRecord>>(
+      equals: isSameDay,
+      hashCode: (DateTime key) {
+        return key.day * 1000000 + key.month * 10000 + key.year;
+      },
+    )..addAll(map);
+
+    return result;
   }
 }
